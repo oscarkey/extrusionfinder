f558e68aa10f3bee9a2f9b1e962ffa07f1794adc
Oscar Key <ok263@cam.ac.uk>
2015-02-05 13:39:08 +0000

    First try at CameraController interface to abstract multiple implementations.

---
+++ client/app/src/main/java/uk/ac/cam/cl/groupecho/extrusionfinder/CameraController.java
@@ -0,0 +1,41 @@
+package uk.ac.cam.cl.groupecho.extrusionfinder;
+
+import android.view.SurfaceView;
+
+/**
+ * Created by oscar on 05/02/15.
+ * An interface to provide an abstraction over the two types of camera available depending on
+ * Android version.
+ */
+public interface CameraController {
+
+    /**
+     * Calls the relevant methods to start the camera.
+     * The camera must first have been setup with a call to setup()
+     * Camera must then be stopped when the activity is paused
+     */
+    public void startCamera();
+
+    /**
+     * Calls the relevant methods to stop the camera.
+     */
+    public void stopCamera();
+
+    /**
+     * Must be called to set up the camera before starting it or taking a picture
+     * @param previewSurface The surface where the preview from the camera should be displayed
+     * @param capturedCallback Called when the image has been fully captured and provides byte data
+     */
+    public void setupCamera(SurfaceView previewSurface, ImageCapturedCallback capturedCallback);
+
+    /**
+     * Implemented to handle the completion of the image capture event.
+     */
+    public interface ImageCapturedCallback {
+        /**
+         * Called when the image has been completely captured
+         * @param image The byte data of the image in a yet to be determined format
+         */
+        public void onImageCaptured(byte[] image);
+    }
+}
