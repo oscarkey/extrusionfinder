21409873d00954c9a638ba13b23ed69ca38ae031
Oscar Key <ok263@cam.ac.uk>
2015-02-06 16:54:23 +0000

    Updated CommsService to allow each request to have a uuid.

--- client/app/src/main/java/uk/ac/cam/cl/echo/extrusionfinder/client/CommsService.java
+++ client/app/src/main/java/uk/ac/cam/cl/echo/extrusionfinder/client/CommsService.java
@@ -13,6 +13,8 @@ public class CommsService extends IntentService {
 
     private static final String EXTRA_IMAGE_BYTES
             = "uk.ac.cam.cl.echo.extrusionfinder.client.extra.IMAGE_BYTES";
+    private static final String EXTRA_REQUEST_UUID
+            = "uk.ac.cam.cl.echo.extrusionfinder.client.extra.REQUEST_UUID";
 
     /**
      * Starts this service to request results with the given image. If
@@ -20,9 +22,10 @@ public class CommsService extends IntentService {
      *
      * @see IntentService
      */
-    public static void startActionRequestResults(Context context, byte[] image) {
+    public static void startActionRequestResults(Context context, String uuid, byte[] image) {
         Intent intent = new Intent(context, CommsService.class);
         intent.setAction(ACTION_REQUEST_RESULTS);
+        intent.putExtra(EXTRA_REQUEST_UUID, uuid);
         intent.putExtra(EXTRA_IMAGE_BYTES, image);
         context.startService(intent);
     }
@@ -36,8 +39,9 @@ public class CommsService extends IntentService {
         if (intent != null) {
             final String action = intent.getAction();
             if (ACTION_REQUEST_RESULTS.equals(action)) {
+                final String uuid = intent.getStringExtra(EXTRA_REQUEST_UUID);
                 final byte[] image = intent.getByteArrayExtra(EXTRA_IMAGE_BYTES);
-                handleActionRequestResults(image);
+                handleActionRequestResults(uuid, image);
             }
         }
     }
@@ -45,7 +49,7 @@ public class CommsService extends IntentService {
     /**
      * Request results for the image. This runs in a provided background thread.
      */
-    private void handleActionRequestResults(byte[] image) {
+    private void handleActionRequestResults(String uuid, byte[] image) {
         //TODO handle request results
         throw new UnsupportedOperationException("Not yet implemented");
     }
