f99357392ebe7822d1af106958be691e296087c1
Oscar Key <ok263@cam.ac.uk>
2015-02-09 20:21:25 +0000

    Added fake result fetching and delivering.

--- client/app/src/main/java/uk/ac/cam/cl/echo/extrusionfinder/client/CommsService.java
+++ client/app/src/main/java/uk/ac/cam/cl/echo/extrusionfinder/client/CommsService.java
@@ -1,13 +1,23 @@
 package uk.ac.cam.cl.echo.extrusionfinder.client;
 
 import android.app.IntentService;
-import android.content.Intent;
 import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * An {@link IntentService} for submitting an image and loading results from the server
  */
 public class CommsService extends IntentService {
+    private static final String LOG_TAG = "CommsService";
+
+    public static final String ACTION_RESULTS_RECEIVED
+            = "uk.ac.cam.cl.echo.extrusionfinder.client.action.RESULTS_RECEIVED";
+
     private static final String ACTION_REQUEST_RESULTS
             = "uk.ac.cam.cl.echo.extrusionfinder.client.action.REQUEST_RESULTS";
 
@@ -50,7 +60,23 @@ public class CommsService extends IntentService {
      * Request results for the image. This runs in a provided background thread.
      */
     private void handleActionRequestResults(String uuid, byte[] image) {
-        //TODO handle request results
-        throw new UnsupportedOperationException("Not yet implemented");
+        //TODO get results from the server
+        // for now just wait a bit and then send back some fake results
+        try {
+            Thread.sleep(1500);
+        }
+        catch(InterruptedException e) {
+            Log.e(LOG_TAG, e.toString());
+        }
+
+        List<Result> results = Arrays.asList(new Result("part1"), new Result("part2"));
+        ResultsCache resultsCache = ResultsCache.getInstance(this);
+        resultsCache.putResults(uuid, results);
+
+        // send a local broadcast to notify the ui that we have received the results
+        Intent intent = new Intent(ACTION_RESULTS_RECEIVED);
+
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        localBroadcastManager.sendBroadcast(intent);
     }
 }
