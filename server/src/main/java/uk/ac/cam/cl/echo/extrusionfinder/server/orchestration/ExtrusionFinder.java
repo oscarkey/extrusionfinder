d82cce7329a17506262ae12d9f0f50dc63493369
Alexander Simpson <as2388@cam.ac.uk>
2015-02-17 16:30:21 +0000

    Improved internal comments and interface between Servlet and ExtrusionFinder

--- server/src/main/java/uk/ac/cam/cl/echo/extrusionfinder/server/orchestration/ExtrusionFinder.java
+++ server/src/main/java/uk/ac/cam/cl/echo/extrusionfinder/server/orchestration/ExtrusionFinder.java
@@ -25,7 +25,7 @@ public class ExtrusionFinder implements IExtrusionFinder {
 
         @Override
         public int compareTo(CorrelationPair o) {
-            return this.correlation - o.correlation > 0 ? 1 : -1;
+            return this.correlation - o.correlation > 0 ? -1 : 1;
         }
     }
 
@@ -33,22 +33,37 @@ public class ExtrusionFinder implements IExtrusionFinder {
     @Override
     public List<MatchedPart> findMatches(BufferedImage inputImage, IDBManager database, int maxResults)
             throws ItemNotFoundException {
+        // TODO: call preprocessor to clean up inputImage before proceeding
 
-
+        // Load the map of zernike moments from the database
         Set<Map.Entry<String, double[]>> zernikeMoments =
                 database.loadZernikeMap().getZernikeMap().entrySet();
 
-        PriorityQueue<CorrelationPair> bestParts = new PriorityQueue<>();
+        // Create a priority queue into which the ids of the parts most matching inputImage will be sorted by
+        // their correlation. But simply doing this may result in a very large queue into which inserting becomes
+        // expensive, and has high memory requirements. So instead items in this queue are sorted in reverse order,
+        // so that when the queue reaches the size of maxResults, the worst item can be removed in constant time.
+        PriorityQueue<CorrelationPair> bestParts = new PriorityQueue<>(maxResults + 1);
 
+        // Compare the input image with every zernike moment just loaded from the database, and put the result
+        // in the priority queue. Having done this, if the priority queue is bigger than the requested number of
+        // results, remove the top of the priority queue (which is the worst value).
         for (Map.Entry<String, double[]> moment: zernikeMoments) {
             double correlation = ImageMatcher.compare(inputImage, moment.getValue());
             bestParts.add(new CorrelationPair(moment.getKey(), correlation));
+
+            if (bestParts.size() > maxResults) bestParts.poll();
         }
 
+        // Determine how many results should be returned: this is the maximum of the number of results requested
+        // and the number of parts in the database
         if (zernikeMoments.size() < maxResults) {
             maxResults = zernikeMoments.size();
         }
 
+        // Repeat (number of parts to return) times:
+        // Take the best item from the priority queue, load its full part data from the database, and insert it
+        // into the result list
         List<MatchedPart> matches = new ArrayList<>(maxResults);
         for (int i = 0; i < maxResults; i++) {
             CorrelationPair pair = bestParts.poll();
@@ -56,6 +71,22 @@ public class ExtrusionFinder implements IExtrusionFinder {
             matches.add(new MatchedPart(part, pair.correlation));
         }
 
-        return matches;
+        // Return the best matches. Because the priority queue was in reverse order, matches is in reverse order
+        // of best matches, so needs to be reversed first.
+        return reverse(matches);
+    }
+
+    /**
+     * Returns a new List, which is the input list in reverse
+     * @param x List to reverse
+     * @return  x in reverse order
+     */
+    private List<MatchedPart> reverse(List<MatchedPart> x) {
+        List<MatchedPart> output = new ArrayList<>(x.size());
+        for (int i = 1; i <= x.size(); i++) {
+            output.add(x.get(x.size() - i));
+        }
+
+        return output;
     }
 }
