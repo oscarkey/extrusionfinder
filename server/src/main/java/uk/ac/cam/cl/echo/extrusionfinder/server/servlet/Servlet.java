2f7efe7466f3823362ef7b24f3d1a67e2a12d7f0
Alexander Simpson <as2388@cam.ac.uk>
2015-02-17 16:30:21 +0000

    Improved internal comments and interface between Servlet and ExtrusionFinder

--- server/src/main/java/uk/ac/cam/cl/echo/extrusionfinder/server/servlet/Servlet.java
+++ server/src/main/java/uk/ac/cam/cl/echo/extrusionfinder/server/servlet/Servlet.java
@@ -1,9 +1,16 @@
 package uk.ac.cam.cl.echo.extrusionfinder.server.servlet;
 
+import uk.ac.cam.cl.echo.extrusionfinder.server.configuration.Configuration;
 import uk.ac.cam.cl.echo.extrusionfinder.server.database.ItemNotFoundException;
+import uk.ac.cam.cl.echo.extrusionfinder.server.database.MongoDBManager;
+import uk.ac.cam.cl.echo.extrusionfinder.server.orchestration.ExtrusionFinder;
 import uk.ac.cam.cl.echo.extrusionfinder.server.parts.MatchedPart;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 /**
@@ -16,25 +23,14 @@ public class Servlet implements IServlet {
      */
     @Override
     public List<MatchedPart> findMatches(String encodedImage) throws IOException, ItemNotFoundException {
-        // TODO: set image to decoded version of encodedImage
-//        byte[] imageData = Base64.getDecoder().decode(encodedImage);
-//        BufferedImage image = ImageIO.read(new ByteArrayInputStream(imageData));
-//
-//        // TODO: Be prepared to call the image processor if filtering is to be done server side
-//
-//        IExtrusionFinder matcher = new ExtrusionFinder(); // TODO: set to a concrete implementer of IExtrusionMatcher
-//        return matcher.findMatches(image, new MongoDBManager("extrusionDB"), 10,
-//                new ZernikeManager(new MongoDBManager("extrusionDB")).getZernikeMoments());
+        // Decode image from base64 encoding to a BufferedImage via a byte array
+        byte[] imageData = Base64.getDecoder().decode(encodedImage);
+        BufferedImage image = ImageIO.read(new ByteArrayInputStream(imageData));
 
-
-        return null;
-        //return null;
-        // TODO: remove
-//        List<MatchedPart> matches = new LinkedList<>();
-//
-//        matches.add(new MatchedPart(new Part("SG00", "SG", "00", "a_l", "http://"), 0.1));
-//        matches.add(new MatchedPart(new Part("SG01", "SG", "01", "b_l", "http://"), 0.3));
-//
-//        return matches;
+        // Find and return the best matches
+        return new ExtrusionFinder().findMatches(
+                image, new MongoDBManager(Configuration.DEFAULT_DATABASE_NAME),
+                Configuration.DEFAULT_NUMBER_OF_MATCHES
+        );
     }
 }
