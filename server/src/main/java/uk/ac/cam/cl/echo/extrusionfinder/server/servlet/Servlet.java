e2f4cf51117e777015abe6b9f763e4e86a02753b
Alexander Simpson <as2388@cam.ac.uk>
2015-02-19 11:07:44 +0000

    Returning dummy data

--- server/src/main/java/uk/ac/cam/cl/echo/extrusionfinder/server/servlet/Servlet.java
+++ server/src/main/java/uk/ac/cam/cl/echo/extrusionfinder/server/servlet/Servlet.java
@@ -5,12 +5,14 @@ import uk.ac.cam.cl.echo.extrusionfinder.server.database.ItemNotFoundException;
 import uk.ac.cam.cl.echo.extrusionfinder.server.database.MongoDBManager;
 import uk.ac.cam.cl.echo.extrusionfinder.server.orchestration.ExtrusionFinder;
 import uk.ac.cam.cl.echo.extrusionfinder.server.parts.MatchedPart;
+import uk.ac.cam.cl.echo.extrusionfinder.server.parts.Part;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Base64;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -23,14 +25,22 @@ public class Servlet implements IServlet {
      */
     @Override
     public List<MatchedPart> findMatches(String encodedImage) throws IOException, ItemNotFoundException {
+
+        System.out.println("Endpoint hit");
         // Decode image from base64 encoding to a BufferedImage via a byte array
         byte[] imageData = Base64.getDecoder().decode(encodedImage);
         BufferedImage image = ImageIO.read(new ByteArrayInputStream(imageData));
 
         // Find and return the best matches
-        return ExtrusionFinder.findMatches(
-                image, new MongoDBManager(Configuration.DEFAULT_DATABASE_NAME),
-                Configuration.DEFAULT_NUMBER_OF_MATCHES
-        );
+//        return ExtrusionFinder.findMatches(
+//                image, new MongoDBManager(Configuration.DEFAULT_DATABASE_NAME),
+//                Configuration.DEFAULT_NUMBER_OF_MATCHES
+//        );
+
+        System.out.println("returning");
+        List<MatchedPart> results = new LinkedList<>();
+        results.add(new MatchedPart(new Part("SG00", "SG", "00", "link", "imagelink"), 0.05));
+        results.add(new MatchedPart(new Part("SG01", "SG", "01", "link", "imagelink"), 0.09));
+        return results;
     }
 }
