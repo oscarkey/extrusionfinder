66ad4d7b019ddf2d7e8863cb7d7422a25d06b019
Ashley Newson <ajn44@cam.ac.uk>
2015-02-21 15:21:35 +0000

    Improved JavaDoc for ImageData clesses

--- server/src/main/java/uk/ac/cam/echo/extrusionfinder/imagedata/GrayscaleImageData.java
+++ server/src/main/java/uk/ac/cam/echo/extrusionfinder/imagedata/GrayscaleImageData.java
@@ -3,11 +3,18 @@ package uk.ac.cam.echo.extrusionfinder.imagedata;
 /**
  * Image storing RGB colour space in bytes.
  * <p>
- * Pixels go R, G, B, R, G, B, R, G, B, ...
+ * Bytes store data for pixels as R, G, B, R, G, B, R, G, B, ...
  * <p>
  * Pixels are stored row by row in left-to-right, top-to-bottom order.
  */
 public class GrayscaleImageData extends ImageData<byte[]> {
+    /**
+     * Construct an image with the given byte data, width and height.
+     *
+     * @param data (Reference) to image data. Other classes are responsible for initialising data.
+     * @param width The width in pixels (not colour elements or subpixels).
+     * @param height The height in pixels (not colour elements or subpixels).
+     */
     public GrayscaleImageData(byte[] data, int width, int height) {
         super(data, width, height);
     }
