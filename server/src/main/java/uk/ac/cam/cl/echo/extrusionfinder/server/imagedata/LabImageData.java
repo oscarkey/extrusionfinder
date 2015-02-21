fb30ae2ac47363573365e484e8450ef04b7ee471
Joshua Landau <joshua@landau.ws>
2015-02-21 22:49:08 +0000

    Added colorspace conversions and tests

---
+++ server/src/main/java/uk/ac/cam/cl/echo/extrusionfinder/server/imagedata/LabImageData.java
@@ -0,0 +1,21 @@
+package uk.ac.cam.cl.echo.extrusionfinder.server.imagedata;
+
+/**
+ * Image storing an L*a*b* colour space image in doubles.
+ * <p>
+ * Bytes store data for pixels as L*, a*, b*, L*, a*, b*, L*, a*, b*, ...
+ * <p>
+ * Pixels are stored row by row in left-to-right, top-to-bottom order.
+ */
+public class LabImageData extends ImageData<double[]> {
+    /**
+     * Construct an image with the given RGB double data, width and height.
+     *
+     * @param data (Reference) to image data. Other classes are responsible for initialising data.
+     * @param width The width in pixels (not colour elements or subpixels).
+     * @param height The height in pixels (not colour elements or subpixels).
+     */
+    public LabImageData(double[] data, int width, int height) {
+        super(data, width, height);
+    }
+}
---
+++ server/src/main/java/uk/ac/cam/cl/echo/extrusionfinder/server/imagedata/XYZImageData.java
@@ -0,0 +1,21 @@
+package uk.ac.cam.cl.echo.extrusionfinder.server.imagedata;
+
+/**
+ * Image storing an XYZ colour space image in doubles.
+ * <p>
+ * Bytes store data for pixels as X, Y, Z, X, Y, Z, X, Y, Z, ...
+ * <p>
+ * Pixels are stored row by row in left-to-right, top-to-bottom order.
+ */
+public class XYZImageData extends ImageData<double[]> {
+    /**
+     * Construct an image with the given RGB double data, width and height.
+     *
+     * @param data (Reference) to image data. Other classes are responsible for initialising data.
+     * @param width The width in pixels (not colour elements or subpixels).
+     * @param height The height in pixels (not colour elements or subpixels).
+     */
+    public XYZImageData(double[] data, int width, int height) {
+        super(data, width, height);
+    }
+}
